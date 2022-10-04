f1f57d15a9b36cdabe46ff5e809d29e85bd7a0f5
Daniel Almeida <dborgesalmeida@gmail.com>
2022-10-04 18:11:28 -0300

    implementacao-js

---
+++ scripts.js
@@ -0,0 +1,14 @@
+const modalOverlay = document.querySelector(".modal-overlay")
+const itens = document.querySelector(".itens")
+
+for (item of itens) {
+    item.addEventListener("click", function(){
+
+    })
+}
+
+// O QUE EU PRECISO FAZER:
+
+// UMA ESTRUTURA QUE PUXA A IMAGEM, O TÍTULO E O SUBTÍTULO DO ITEM PARA O MODAL
+
+// UMA ESTRUTURA QUE, AO APERTAR EM "FECHAR MODAL", ELE REMOVA TODAS AS CLASSES E FECHE O MODAL
